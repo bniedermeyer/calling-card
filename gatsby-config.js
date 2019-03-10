514ca12dbbf1b856a87f95353aa776ede8097e87
Brenden <brenden.niedermeyer@gmail.com>
2019-03-10 12:53:32 -0700

    feat: setup deploy and GA

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-136002182-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
+  ],
+}
--- src/components/link-button/link-button.js
+++ src/components/link-button/link-button.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { OutboundLink } from "gatsby-plugin-google-analytics"
+
 import linkButtonStyles from "./link-button.module.css"
 
 export default props => (
-  <a className={linkButtonStyles.linkButton} href={props.url}>
+  <OutboundLink className={linkButtonStyles.linkButton} href={props.url}>
     {props.title}
-  </a>
+  </OutboundLink>
 )
